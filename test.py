48d8c6fef9f7c54e38889fd1a0d826d7882b0539
Ayando Yarde <Ayando.Yarde@student.csulb.edu>
2016-02-23 22:25:55 -0800

    testing

---
+++ test.py
@@ -0,0 +1,8 @@
+name = input("What is your name?")
+quest = input("What is your quest?")
+color = input("What is your favorite color?")
+
+print("poop")
+print ("Ah, so your name is %s, your quest is %s, ")
+print ("and your favorite color is %s." % (str(name), str(quest), str(color))
+
